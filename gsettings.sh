0c357b460a11de431ef9ef2b8b41318dc76f10de
Nikola Stoyanov <ns.barzakov@gmail.com>
2019-03-31 14:57:08 +0300

    Fix duplicated keybindings in Gnome

--- gsettings.sh
+++ gsettings.sh
@@ -2,22 +2,44 @@
 
 # Gnome 3 configuration
 
-# Switch windows instead of application groups
-gsettings set org.gnome.desktop.wm.keybindings switch-applications '[]'
-gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Super>Tab', '<Alt>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Super>Tab', '<Shift><Alt>Tab']"
-
 # Customize workspaces
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
 
-# Disable "Activities" shortcut so that '<Super>' alone doesn't do anything
-gsettings set org.gnome.mutter overlay-key ""
-
 # Set "Nigth Light" temperature
 gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 5000
+
+# Keybindings
+## Switch windows instead of application groups
+gsettings set org.gnome.desktop.wm.keybindings switch-applications '[]'
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Super>Tab', '<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Super>Tab', '<Shift><Alt>Tab']"
+
+## Set navigation keybinds and remove conflicts
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>1']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Super><Shift>2']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Super><Shift>3']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Super><Shift>4']"
+
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
+
+# Disable "Activities" shortcut so that '<Super>' alone doesn't do anything
+gsettings set org.gnome.mutter overlay-key ""
 
 # TODO: Stop sounds when using the volume-up/volume-down keys.
 # They can be disabled from the GUI: Settings > Sound Effects
